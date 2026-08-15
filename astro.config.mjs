2bbb899030a1999efe51d76f680e13363ef418cd
hermano-padawan <lluisjvi@gmail.com>
2026-08-15 16:32:37 +0200

    Configure production domain URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: process.env.GITHUB_ACTIONS
-    ? "https://hermano-padawan.github.io"
-    : "https://biblenexus.site",
-  base: process.env.GITHUB_ACTIONS ? "/bible-nexus" : "/",
+  site: "https://biblenexus.site",
+  base: "/",
   trailingSlash: "always",
   integrations: [sitemap()],
 });
